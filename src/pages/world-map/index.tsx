70a583028219d5d8bad35ef551e89ca0c559414d
Vagner <vagnerlucion@gmail.com>
2022-04-08 21:57:05 -0300

    first commit

---
+++ src/pages/world-map/index.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+import "./index.css"
+
+const WorldMap = () => {
+
+    useEffect(() => {
+
+        // Create root and chart
+        var root = am5.Root.new("chartdiv");
+
+        // Set themes
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+
+        var chart = root.container.children.push(
+            am5map.MapChart.new(root, {
+                panX: "rotateX",
+                projection: am5map.geoNaturalEarth1()
+            })
+        );
+
+        // Create polygon series
+        var polygonSeries = chart.series.push(
+            am5map.MapPolygonSeries.new(root, {
+                geoJSON: am5geodata_worldLow,
+                exclude: ["AQ"]
+            })
+        );
+
+        polygonSeries.mapPolygons.template.setAll({
+            tooltipText: "{name}",
+            interactive: true
+        });
+
+        polygonSeries.mapPolygons.template.states.create("hover", {
+            fill: am5.color(0x677935)
+        });
+
+        chart.events.on("click", (ev) => {
+            console.log(ev)
+            console.log(chart.invert(ev.point))
+        });
+    }, [])
+
+    return (
+        <>
+            <div id="chartdiv"></div>
+        </>
+    )
+
+}
+
+export default WorldMap
